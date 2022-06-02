fd9a2ec4b5658b1d14bb9a56bab6c7a6c080f202
ESDUNCAN <d.eric@rogers.com>
2022-06-02 20:12:12 +0000

    refactored LighthousBnb reservations

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -91,7 +91,21 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+    .query(`SELECT reservations.*, properties.*, avg(property_reviews.rating) as average_rating
+            FROM reservations
+            JOIN properties ON properties.id = property_id
+            JOIN property_reviews ON properties.id = property_reviews.property_id
+            WHERE reservations.guest_id = $1
+            GROUP BY reservations.id, properties.id
+            ORDER BY reservations.start_date
+            LIMIT 10;`, [guest_id])
+    .then((result) => {
+      return result.rows
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 }
 exports.getAllReservations = getAllReservations;
 
